591760ba217051846e5e3841e473befca2603019
Andor Szabó <szandor.92@gmail.com>
2023-12-07 22:38:01 +0200

    created and called fizz_buzz function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 
 fn main() {
-    println!("Welcome to my first Rust project!")
+    println!("Welcome to my first Rust project!");
+    fizz_buzz()
+}
+
+fn fizz_buzz() {
+    for n in 1..=301 {
+        match n % 3 {
+            0 => println!(" fizz {}", &n),
+            _ => {}
+        }
+    }
 }
 
