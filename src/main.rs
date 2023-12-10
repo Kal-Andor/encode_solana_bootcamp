73a5df62e7ed56783a17811e38d81e66f0c49a4f
Andor Szabó <szandor.92@gmail.com>
2023-12-10 18:31:43 +0200

    implemented constants

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+const FIZZ:&str = "fizz";
+const BUZZ:&str = "buzz";
+const FIZZ_BUZZ:&str = "fizz-buzz";
 
 fn main() {
     println!("Welcome to my first Rust project!");
@@ -8,15 +11,16 @@ fn fizz_buzz() {
     let mut counter:i32 =0;
     for n in 1..=301 {
         let word:Option<&str> =
-            if n % 15 == 0 { Some("fizz-buzz") }
-            else if n % 3 == 0 { Some("fizz") }
-            else if n % 5 == 0 { Some("buzz") }
+            if n % 15 == 0 { Some(FIZZ_BUZZ) }
+            else if n % 3 == 0 { Some(FIZZ) }
+            else if n % 5 == 0 { Some(BUZZ) }
             else { None };
         match word {
-            Some("fizz-buzz") => {println!("fizz-buzz"); counter += 1}
+            Some(FIZZ_BUZZ) => {println!("{}", FIZZ_BUZZ); counter += 1}
             Some(result) => {println!("{}", result)}
             None => {}
         }
     }
     println!("Number of 'fizz-buzz'-s:{}", counter)
 }
+
