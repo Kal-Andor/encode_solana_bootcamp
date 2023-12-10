25211e4617df2a9b247198841f1b7ddaacf03dd3
Andor Szabó <szandor.92@gmail.com>
2023-12-10 16:51:46 +0200

    solved the problem using if elseif expression

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ fn main() {
 
 fn fizz_buzz() {
     for n in 1..=301 {
-        match n % 3 {
-            0 => println!(" fizz {}", &n),
-            _ => {}
-        }
+        // match n % 3 {
+        //     0 => println!(" fizz {}", &n),
+        //     _ => {}
+        // }
+        let word = if n % 15 == 0 { "fizz-buzz" } else if n % 3 == 0 { "fizz" } else if n % 5 == 0 { "buzz" } else { "" };
+        println!("{} {}", word, n)
     }
 }
 
